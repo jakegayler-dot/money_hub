f08339f8b9abfa593354250f81b2655733a2f7bc
Jake <jake.gayler@gmail.com>
2026-09-17 11:48:02 -0600

    Enable sourcemaps for debugging

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+﻿import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
